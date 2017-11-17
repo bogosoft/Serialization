9ecf8dad5647dc8e7c5de3b2751fbc348900354f
Clayton Roth <croth0627@yahoo.com>
2017-11-16 23:53:26 -0800

    added JSON-specific serialization project

---
+++ Bogosoft.Serialization.Json/JsonSerializer.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bogosoft.Serialization.Json
+{
+    public sealed class JsonSerializer :
+        IAsyncDeserializer<TextReader>,
+        IAsyncSerializer<TextWriter>,
+        IDeserializer<TextReader>,
+        ISerializer<TextWriter>
+    {
+        Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+
+        public object Deserialize(TextReader source)
+        {
+            using (var reader = new JsonTextReader(source))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+
+        public Task<object> DeserializeAsync(TextReader source, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            using (var reader = new JsonTextReader(source))
+            {
+                return Task.FromResult(serializer.Deserialize(reader));
+            }
+        }
+
+        public void Serialize(object data, TextWriter destination)
+        {
+            using (var writer = new JsonTextWriter(destination))
+            {
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        public Task SerializeAsync(object data, TextWriter destination, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            using (var writer = new JsonTextWriter(destination))
+            {
+                serializer.Serialize(writer, data);
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
