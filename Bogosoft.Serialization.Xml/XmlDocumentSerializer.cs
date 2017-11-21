bfc4554aa9cca2c625dcf4a65911892e35afaa08
Clayton Roth <croth0627@yahoo.com>
2017-11-20 23:10:30 -0800

    alphabetized methods

--- Bogosoft.Serialization.Xml/XmlDocumentSerializer.cs
+++ Bogosoft.Serialization.Xml/XmlDocumentSerializer.cs
@@ -33,6 +33,18 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML node to a given text writer.
+        /// </summary>
+        /// <param name="data">An XML node to serialize.</param>
+        /// <param name="destination">A destination to serialize to.</param>
+        /// <param name="token">A cancellation instruction.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
+        public Task SerializeAsync(XmlNode data, TextWriter destination, CancellationToken token)
+        {
+            return SerializeNodeAsync(data, destination, "", token);
+        }
+
+        /// <summary>
         /// Serialize a given XML attribute.
         /// </summary>
         /// <param name="attribute">An XML attribute to serialize.</param>
@@ -46,6 +58,26 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML attribute to a given text writer.
+        /// </summary>
+        /// <param name="attribute">An XML attribute to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML attribute to.</param>
+        /// <param name="indent">
+        /// A value corresponding to the current level of indentation.
+        /// </param>
+        /// <param name="token">A cancellation instruction.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
+        protected virtual Task SerializeAttributeAsync(
+            XmlAttribute attribute,
+            TextWriter output,
+            string indent,
+            CancellationToken token
+            )
+        {
+            return output.WriteAsync($@" {attribute.Name}=""{attribute.Value}""", token);
+        }
+
+        /// <summary>
         /// Serialize a given XML CDATA section.
         /// </summary>
         /// <param name="cdata">An XML CDATA section to serialize.</param>
@@ -56,6 +88,22 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML CDATA section to a given text writer.
+        /// </summary>
+        /// <param name="cdata">An XML CDATA section to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML CDATA section to.</param>
+        /// <param name="token">A cancellation instruction.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
+        protected virtual Task SerializeCDataSectionAsync(
+            XmlCDataSection cdata,
+            TextWriter output,
+            CancellationToken token
+            )
+        {
+            return output.WriteAsync(cdata.OuterXml, token);
+        }
+
+        /// <summary>
         /// Serialize a given XML comment.
         /// </summary>
         /// <param name="comment">An XML comment to serialize.</param>
@@ -63,6 +111,22 @@ namespace Bogosoft.Serialization.Xml
         protected virtual void SerializeComment(XmlComment comment, TextWriter output)
         {
             output.Write(comment.OuterXml);
+        }
+
+        /// <summary>
+        /// Serialize a given XML comment to a given text writer.
+        /// </summary>
+        /// <param name="comment">An XML comment to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML comment to.</param>
+        /// <param name="token">A cancellation instruction.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
+        protected virtual Task SerializeCommentAsync(
+            XmlComment comment,
+            TextWriter output,
+            CancellationToken token
+            )
+        {
+            return output.WriteAsync(comment.OuterXml, token);
         }
 
         /// <summary>
@@ -92,202 +156,6 @@ namespace Bogosoft.Serialization.Xml
             {
                 SerializeNode(child, output, indent);
             }
-        }
-
-        /// <summary>
-        /// Serialize a given XML document type.
-        /// </summary>
-        /// <param name="doctype">An XML document type to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML document type to.</param>
-        protected virtual void SerializeDocumentType(XmlDocumentType doctype, TextWriter output)
-        {
-            output.Write(doctype.OuterXml);
-        }
-
-        /// <summary>
-        /// Serialize a given XML element.
-        /// </summary>
-        /// <param name="element">An XML element to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML element to.</param>
-        /// <param name="indent">
-        /// A value corresponding to the current level of indentation.
-        /// </param>
-        protected virtual void SerializeElement(XmlElement element, TextWriter output, string indent)
-        {
-            output.Write(LBreak + indent + "<" + element.Name);
-
-            foreach (XmlAttribute a in element.Attributes)
-            {
-                SerializeAttribute(a, output, indent);
-            }
-
-            if (!element.HasChildNodes)
-            {
-                output.Write(" />");
-
-                return;
-            }
-
-            var ecount = 0;
-
-            foreach (XmlNode child in element.ChildNodes)
-            {
-                SerializeNode(child, output, indent + Indent);
-
-                if (child.NodeType == XmlNodeType.Element)
-                {
-                    ++ecount;
-                }
-            }
-
-            if (ecount > 0)
-            {
-                output.Write(LBreak + indent);
-            }
-
-            output.Write($"</{element.Name}>");
-        }
-
-        /// <summary>
-        /// Serialize a given XML node.
-        /// </summary>
-        /// <param name="node">An XML node to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML node to.</param>
-        /// <param name="indent">
-        /// A value corresponding to the current level of indentation.
-        /// </param>
-        protected virtual void SerializeNode(XmlNode node, TextWriter output, string indent)
-        {
-            switch (node.NodeType)
-            {
-                case XmlNodeType.Attribute:
-                    SerializeAttribute(node as XmlAttribute, output, indent);
-                    break;
-                case XmlNodeType.CDATA:
-                    SerializeCDataSection(node as XmlCDataSection, output);
-                    break;
-                case XmlNodeType.Comment:
-                    SerializeComment(node as XmlComment, output);
-                    break;
-                case XmlNodeType.Document:
-                    SerializeDocument(node as XmlDocument, output);
-                    break;
-                case XmlNodeType.DocumentFragment:
-                    SerializeDocumentFragment(node as XmlDocumentFragment, output, indent);
-                    break;
-                case XmlNodeType.DocumentType:
-                    SerializeDocumentType(node as XmlDocumentType, output);
-                    break;
-                case XmlNodeType.Element:
-                    SerializeElement(node as XmlElement, output, indent);
-                    break;
-                case XmlNodeType.ProcessingInstruction:
-                    SerializeProcessingInstruction(node as XmlProcessingInstruction, output);
-                    break;
-                case XmlNodeType.Text:
-                    SerializeText(node as XmlText, output);
-                    break;
-                case XmlNodeType.XmlDeclaration:
-                    SerializeXmlDeclaration(node as XmlDeclaration, output);
-                    break;
-                default:
-                    output.Write(node.OuterXml);
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Serialize a given XML processing instruction.
-        /// </summary>
-        /// <param name="pi">An XML processing instruction to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML processing instruction to.</param>
-        protected virtual void SerializeProcessingInstruction(XmlProcessingInstruction pi, TextWriter output)
-        {
-            output.Write(pi.OuterXml);
-        }
-
-        /// <summary>
-        /// Serialize a given XML declaration.
-        /// </summary>
-        /// <param name="declaration">An XML declaration to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML declaration to.</param>
-        protected virtual void SerializeXmlDeclaration(XmlDeclaration declaration, TextWriter output)
-        {
-            output.Write(declaration.OuterXml);
-        }
-
-        /// <summary>
-        /// Serialize a given text XML node.
-        /// </summary>
-        /// <param name="text">An XML text node to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML text node to.</param>
-        protected virtual void SerializeText(XmlText text, TextWriter output)
-        {
-            output.Write(text.Data);
-        }
-
-        /// <summary>
-        /// Serialize a given XML node to a given text writer.
-        /// </summary>
-        /// <param name="data">An XML node to serialize.</param>
-        /// <param name="destination">A destination to serialize to.</param>
-        /// <param name="token">A cancellation instruction.</param>
-        /// <returns>A task representing an asynchronous operation.</returns>
-        public Task SerializeAsync(XmlNode data, TextWriter destination, CancellationToken token)
-        {
-            return SerializeNodeAsync(data, destination, "", token);
-        }
-
-        /// <summary>
-        /// Serialize a given XML attribute to a given text writer.
-        /// </summary>
-        /// <param name="attribute">An XML attribute to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML attribute to.</param>
-        /// <param name="indent">
-        /// A value corresponding to the current level of indentation.
-        /// </param>
-        /// <param name="token">A cancellation instruction.</param>
-        /// <returns>A task representing an asynchronous operation.</returns>
-        protected virtual Task SerializeAttributeAsync(
-            XmlAttribute attribute,
-            TextWriter output,
-            string indent,
-            CancellationToken token
-            )
-        {
-            return output.WriteAsync($@" {attribute.Name}=""{attribute.Value}""", token);
-        }
-
-        /// <summary>
-        /// Serialize a given XML CDATA section to a given text writer.
-        /// </summary>
-        /// <param name="cdata">An XML CDATA section to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML CDATA section to.</param>
-        /// <param name="token">A cancellation instruction.</param>
-        /// <returns>A task representing an asynchronous operation.</returns>
-        protected virtual Task SerializeCDataSectionAsync(
-            XmlCDataSection cdata,
-            TextWriter output,
-            CancellationToken token
-            )
-        {
-            return output.WriteAsync(cdata.OuterXml, token);
-        }
-
-        /// <summary>
-        /// Serialize a given XML comment to a given text writer.
-        /// </summary>
-        /// <param name="comment">An XML comment to serialize.</param>
-        /// <param name="output">A text writer to serialize a given XML comment to.</param>
-        /// <param name="token">A cancellation instruction.</param>
-        /// <returns>A task representing an asynchronous operation.</returns>
-        protected virtual Task SerializeCommentAsync(
-            XmlComment comment,
-            TextWriter output,
-            CancellationToken token
-            )
-        {
-            return output.WriteAsync(comment.OuterXml, token);
         }
 
         /// <summary>
@@ -333,6 +201,16 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML document type.
+        /// </summary>
+        /// <param name="doctype">An XML document type to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML document type to.</param>
+        protected virtual void SerializeDocumentType(XmlDocumentType doctype, TextWriter output)
+        {
+            output.Write(doctype.OuterXml);
+        }
+
+        /// <summary>
         /// Serialize a given XML document type to a given text writer.
         /// </summary>
         /// <param name="doctype">An XML document type to serialize.</param>
@@ -346,6 +224,50 @@ namespace Bogosoft.Serialization.Xml
             )
         {
             return output.WriteAsync(doctype.OuterXml, token);
+        }
+
+        /// <summary>
+        /// Serialize a given XML element.
+        /// </summary>
+        /// <param name="element">An XML element to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML element to.</param>
+        /// <param name="indent">
+        /// A value corresponding to the current level of indentation.
+        /// </param>
+        protected virtual void SerializeElement(XmlElement element, TextWriter output, string indent)
+        {
+            output.Write(LBreak + indent + "<" + element.Name);
+
+            foreach (XmlAttribute a in element.Attributes)
+            {
+                SerializeAttribute(a, output, indent);
+            }
+
+            if (!element.HasChildNodes)
+            {
+                output.Write(" />");
+
+                return;
+            }
+
+            var ecount = 0;
+
+            foreach (XmlNode child in element.ChildNodes)
+            {
+                SerializeNode(child, output, indent + Indent);
+
+                if (child.NodeType == XmlNodeType.Element)
+                {
+                    ++ecount;
+                }
+            }
+
+            if (ecount > 0)
+            {
+                output.Write(LBreak + indent);
+            }
+
+            output.Write($"</{element.Name}>");
         }
 
         /// <summary>
@@ -400,6 +322,54 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML node.
+        /// </summary>
+        /// <param name="node">An XML node to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML node to.</param>
+        /// <param name="indent">
+        /// A value corresponding to the current level of indentation.
+        /// </param>
+        protected virtual void SerializeNode(XmlNode node, TextWriter output, string indent)
+        {
+            switch (node.NodeType)
+            {
+                case XmlNodeType.Attribute:
+                    SerializeAttribute(node as XmlAttribute, output, indent);
+                    break;
+                case XmlNodeType.CDATA:
+                    SerializeCDataSection(node as XmlCDataSection, output);
+                    break;
+                case XmlNodeType.Comment:
+                    SerializeComment(node as XmlComment, output);
+                    break;
+                case XmlNodeType.Document:
+                    SerializeDocument(node as XmlDocument, output);
+                    break;
+                case XmlNodeType.DocumentFragment:
+                    SerializeDocumentFragment(node as XmlDocumentFragment, output, indent);
+                    break;
+                case XmlNodeType.DocumentType:
+                    SerializeDocumentType(node as XmlDocumentType, output);
+                    break;
+                case XmlNodeType.Element:
+                    SerializeElement(node as XmlElement, output, indent);
+                    break;
+                case XmlNodeType.ProcessingInstruction:
+                    SerializeProcessingInstruction(node as XmlProcessingInstruction, output);
+                    break;
+                case XmlNodeType.Text:
+                    SerializeText(node as XmlText, output);
+                    break;
+                case XmlNodeType.XmlDeclaration:
+                    SerializeXmlDeclaration(node as XmlDeclaration, output);
+                    break;
+                default:
+                    output.Write(node.OuterXml);
+                    break;
+            }
+        }
+
+        /// <summary>
         /// Serialize a given XML node to a given text writer.
         /// </summary>
         /// <param name="node">An XML node to serialize.</param>
@@ -439,6 +409,16 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given XML processing instruction.
+        /// </summary>
+        /// <param name="pi">An XML processing instruction to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML processing instruction to.</param>
+        protected virtual void SerializeProcessingInstruction(XmlProcessingInstruction pi, TextWriter output)
+        {
+            output.Write(pi.OuterXml);
+        }
+
+        /// <summary>
         /// Serialize a given XML processing instruction to a given text writer.
         /// </summary>
         /// <param name="pi">An XML processing instruction to serialize.</param>
@@ -455,6 +435,16 @@ namespace Bogosoft.Serialization.Xml
         }
 
         /// <summary>
+        /// Serialize a given text XML node.
+        /// </summary>
+        /// <param name="text">An XML text node to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML text node to.</param>
+        protected virtual void SerializeText(XmlText text, TextWriter output)
+        {
+            output.Write(text.Data);
+        }
+
+        /// <summary>
         /// Serialize a given XML text node to a given text writer.
         /// </summary>
         /// <param name="text">An XML text node to serialize.</param>
@@ -464,6 +454,16 @@ namespace Bogosoft.Serialization.Xml
         protected virtual Task SerializeTextAsync(XmlText text, TextWriter output, CancellationToken token)
         {
             return output.WriteAsync(text.Data, token);
+        }
+
+        /// <summary>
+        /// Serialize a given XML declaration.
+        /// </summary>
+        /// <param name="declaration">An XML declaration to serialize.</param>
+        /// <param name="output">A text writer to serialize a given XML declaration to.</param>
+        protected virtual void SerializeXmlDeclaration(XmlDeclaration declaration, TextWriter output)
+        {
+            output.Write(declaration.OuterXml);
         }
 
         /// <summary>
